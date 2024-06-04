f723ce3cf6c8f4656e90585b1f7613d8a31a148c
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-06-04 13:16:54 +0530

    Deployment Error Fix: first and last name not showing

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,8 +5,8 @@ declare type CreateUserParams = {
     clerkId: string;
     email: string;
     username: string;
-    firstName: string;
-    lastName: string;
+    firstName: string | null;
+    lastName: string | null;
     photo: string;
 };
 
