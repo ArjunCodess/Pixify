be9f486f360acc6ebfb128c85f5018ee9799b311
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-06-05 09:46:09 +0530

    Project Part 2

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-export default function AddTransformationTypePage() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default async function AddTransformationTypePage({ params: { type } }: SearchParamProps) {
+     const { userId } = auth();
+     const transformation = transformationTypes[type];
+
+     if (!userId) redirect('/sign-in')
+
+     const user = await getUserById(userId);
+
+     return (
+          <>
+               <Header
+                    title={transformation.title}
+                    subtitle={transformation.subTitle}
+               />
+
+               <section className="mt-10">
+                    <TransformationForm
+                         action="Add"
+                         userId={user._id}
+                         type={transformation.type as TransformationTypeKey}
+                         creditBalance={user.creditBalance}
+                    />
+               </section>
+          </>
+     )
 }
